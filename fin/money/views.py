fb24fcc759acdffa4aa2945f73f28a98fb410d90
chethan <chethan@chethan.(none)>
2013-01-06 13:32:52 +0530

    Added index template. Need to adapt bootstrap for better UI

--- fin/money/views.py
+++ fin/money/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("My personal finance management software.")
+	context = {}
+	return render(request, 'money/index.html', context) 
