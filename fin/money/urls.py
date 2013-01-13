adef205a3f7fbefffd2cf16b31344f55dada43fc
chethan <chethan@chethan.(none)>
2013-01-13 18:00:39 +0530

    added a url to display all transactions

--- fin/money/urls.py
+++ fin/money/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 from money import views
 
 urlpatterns = patterns('',
+	url('all', views.all, name='all'),
 	url(r'^$', views.index, name='index'),
 )
--- fin/money/views.py
+++ fin/money/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from money.models import Transaction
 
 def index(request):
 	context = {}
 	return render(request, 'money/index.html', context) 
+
+def all(request):
+	transaction_list = Transaction.objects.all();
+	context={'transaction_list' : transaction_list}
+	return render(request, 'money/all.html', context)
+
